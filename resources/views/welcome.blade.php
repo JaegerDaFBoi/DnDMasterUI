9277d4b4235d47af69a586dc2b8dc6feb2ddd4a7
Juan Morales <jcamilo2696@gmail.com>
2023-09-22 18:28:48 -0500

    Welcome page header title and general styles - fixed vite assets include

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>DND Master UI</title>
-    <link rel="stylesheet" href="{{ asset('assets/fonts/Vecna/Vecna-oppx.ttf') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/Vecna/Vecna-oppx.ttf') }}">
     <!-- Styles -->
     <!-- Scripts -->
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
@@ -18,9 +18,28 @@
     <div class="home-page">
         <div class="container-fluid">
             <div class="welcome-header_wrap">
-                <div class="container welcome-header_inner">
-                    <div class="header_content">
-                        <img class="header_logo" src="{{ asset('img/Dungeons-and-Dragons-Logo.png') }}" alt="Dungeons and Dragons">
+                <div class="welcome-header_inner">
+                    <div class="container welcome-header_content">
+                        <img class="welcome-header_logo" src="{{ asset('img/Dungeons-and-Dragons-Logo.png') }}" alt="Dungeons and Dragons">
+                        <div class="welcome-header_title">
+                            <h1>DnD Master UI</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="welcome-content_wrap">
+                <div class="welcome-content_inner">
+                    <div class="container welcome-content">
+                        <div class="row">
+                            <div class="col-6">
+
+                            </div>
+                            <div class="col-6">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    publicDir: 'public/public' ,
     plugins: [
         laravel({
             input: ['resources/css/app.scss', 'resources/js/app.js'],
